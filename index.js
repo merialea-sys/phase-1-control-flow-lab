949b7dd4a7bd67e29af638e3b21856b8e1333f70
merialea <merialea5@gmail.com>
2025-09-04 17:34:23 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+  //this where we can use conditionals given our argument, someValue
+  //don,t forget to return whatever the result is
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  } else if (someValue > 400 && someValue < 2000) {
+    return "That will be twenty bucks.";
+  }else if (someValue >2000 && someValue < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }else if (someValue >2500) {
+    return "No can do.";
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  const cityMessage = city === "NYC" ? "Ok, sounds good." : "No go."
+  return cityMessage;
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  let tipMessage;
+
+  switch (tip) {
+    case 'generous':
+      tipMessage = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipMessage = 'Thank you.';
+      break;
+    default:
+      tipMessage = 'Bye.';
+  }
+  return tipMessage;
   // Write your code here!
 }
